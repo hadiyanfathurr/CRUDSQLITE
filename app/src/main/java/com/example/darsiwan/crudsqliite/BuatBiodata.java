9ab26f758428d27a02a92058fc7414eb13a8da04
dynfath <hadi.rafi0324@gmail.com>
2018-12-20 23:55:39 +0700

    add new

---
+++ app/src/main/java/com/example/darsiwan/crudsqliite/BuatBiodata.java
@@ -0,0 +1,66 @@
+package com.example.darsiwan.crudsqliite;
+
+
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class BuatBiodata extends AppCompatActivity {
+    protected Cursor cursor;
+    DataHelper dbHelper;
+    Button ton1, ton2;
+    EditText text1, text2, text4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buat_biodata);
+
+        Calendar calendar = Calendar.getInstance();
+        final String currentDate = DateFormat.getDateInstance(DateFormat.DATE_FIELD).format(calendar.getTime());
+
+        dbHelper = new DataHelper(this);
+        text1 = (EditText) findViewById(R.id.editText1);
+        text2 = (EditText) findViewById(R.id.editText2);
+
+        text4 = (EditText) findViewById(R.id.editText4);
+        ton1 = (Button) findViewById(R.id.button1);
+        ton2 = (Button) findViewById(R.id.button2);
+
+        ton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into biodata(no, nama, tgl, jk) values('" +
+                        text1.getText().toString() + "','" +
+                        text2.getText().toString() + "','" +
+                        currentDate + "','" +
+                        text4.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Berhasil", Toast.LENGTH_LONG).show();
+                MainActivity.ma.RefreshList();
+                finish();
+            }
+        });
+        ton2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+
+
+
+}
